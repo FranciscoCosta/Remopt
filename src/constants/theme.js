1eb4d241bfda79ad311f2076e45c74303235a661
Francisco Costa <francisco100eg@gmail.com>
2023-08-03 08:16:17 -0300

    :Feat: Added theme with colors and font-sizes

---
+++ src/constants/theme.js
@@ -0,0 +1,16 @@
+const COLORS = {
+  red: '#f70452',
+  yellow: '#f70452',
+  gray: '#d5dce7',
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export {COLORS, SIZES};
